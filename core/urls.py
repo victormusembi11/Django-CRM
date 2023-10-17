881c8d71a09e5e8e44cbce8bed67424a2712c8ae
victormusembi11 <victor.musembi11@gmail.com>
2023-10-17 10:39:06 +0300

    feat: about page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from core.views import home
+from core.views import home, about
+
+app_name = "core"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("about/", about, name="about"),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def home(request):
     """Home view."""
     return render(request, "core/index.html")
+
+
+def about(request):
+    """About view."""
+    return render(request, "core/about.html")
