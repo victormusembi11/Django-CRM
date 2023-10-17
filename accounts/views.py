aea4d7d989327f0248c38a8ff3c51dd83df7e11c
victormusembi11 <victor.musembi11@gmail.com>
2023-10-17 14:41:54 +0300

    feat: initialised signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,9 @@
 """Accounts views."""
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+
+def signup(request):
+    """User create form view."""
+    form = UserCreationForm()
+    return render(request, "accounts/signup.html", {"form": form})
