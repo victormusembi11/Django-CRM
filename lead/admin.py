e09db566ab4e275296a7c1be3f24fe01ee735acb
victormusembi11 <victor.musembi11@gmail.com>
2023-10-18 08:23:44 +0300

    feat: initialised lead app

---
+++ lead/admin.py
@@ -0,0 +1,1 @@
+"""Admin configuration for lead app."""
---
+++ lead/apps.py
@@ -0,0 +1,9 @@
+"""Lead app config."""
+from django.apps import AppConfig
+
+
+class LeadConfig(AppConfig):
+    """Lead app config."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "lead"
