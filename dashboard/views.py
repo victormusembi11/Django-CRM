817251e03c25d0b48650687c5a77c3d34029eefd
victormusembi11 <victor.musembi11@gmail.com>
2023-10-18 06:04:14 +0300

    feat: added login required decorator to dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 """Dashboard views."""
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def dashboard_view(request):
     """Dashboard view."""
     return render(request, "dashboard/dashboard.html")
