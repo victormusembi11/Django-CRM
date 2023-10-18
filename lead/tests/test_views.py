b844fc20f858f1030e0033c4703b4ed0c28d7be2
victormusembi11 <victor.musembi11@gmail.com>
2023-10-18 18:06:27 +0300

    feat: create lead and redirect to dashboard

--- lead/tests/test_views.py
+++ lead/tests/test_views.py
@@ -9,3 +9,17 @@ def test_add_lead(auth_client):
     response = auth_client.get("/lead/add/")
     assert response.status_code == 200
     assert "lead/add_lead.html" in (t.name for t in response.templates)
+
+
+def test_add_lead_post(auth_client):
+    """Test add lead post view."""
+    data = {
+        "name": "test",
+        "email": "johndoe@example.com",
+        "description": "test",
+        "priority": "low",
+        "status": "new",
+    }
+    response = auth_client.post("/lead/add/", data=data)
+    assert response.status_code == 302
+    assert response.url == "/dashboard/"
--- lead/views.py
+++ lead/views.py
@@ -1,6 +1,6 @@
 """Lead app views."""
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from lead.forms import AddLeadForm
 
@@ -8,5 +8,16 @@ from lead.forms import AddLeadForm
 @login_required
 def add_lead(request):
     """Add lead view."""
+    if request.method == "POST":
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)  # set to false to set created_by
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect("dashboard:dashboard")
+
     form = AddLeadForm()
+
     return render(request, "lead/add_lead.html", {"form": form})
