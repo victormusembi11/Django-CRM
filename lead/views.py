f1f3c7ab3ec4af32e1b6983076f3861887dd0f38
victormusembi11 <victor.musembi11@gmail.com>
2023-10-18 17:57:19 +0300

    feat: initialised add lead form

--- lead/views.py
+++ lead/views.py
@@ -2,8 +2,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from lead.forms import AddLeadForm
+
 
 @login_required
 def add_lead(request):
     """Add lead view."""
-    return render(request, "lead/add_lead.html")
+    form = AddLeadForm()
+    return render(request, "lead/add_lead.html", {"form": form})
