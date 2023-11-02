4557a60801827004b0f213100a9d2912a6bd7088
victormusembi11 <victor.musembi11@gmail.com>
2023-11-02 10:27:48 +0300

    fix: duplicate func names for req/res & model client

--- client/tests/conftest.py
+++ client/tests/conftest.py
@@ -1,14 +1,34 @@
 """Client fixtures."""
 import pytest
 from django.contrib.auth.models import User
+from django.test import Client as TestClient
 
 from client.models import Client
 
 
 @pytest.fixture
-def user(db):
-    """Create user."""
-    return User.objects.create_user(username="testuser", password="123")
+def default_client():
+    """Test client."""
+    return TestClient()
+
+
+@pytest.fixture
+def user():
+    """Test user."""
+    user = User.objects.create_user(
+        username="janedoe",
+        email="janedoe@example.com",
+    )
+    user.set_password("password")
+    user.save()
+    return user
+
+
+@pytest.fixture
+def auth_client(default_client, user):
+    """Test client with authenticated user."""
+    default_client.login(username=user.username, password="password")
+    return default_client
 
 
 @pytest.fixture
