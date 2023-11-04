262dd2de5bd946d4f8ca77f4f449d856453acaaf
victormusembi11 <victor.musembi11@gmail.com>
2023-11-04 15:09:48 +0300

    fix: added login required decorator to client list view

--- client/views.py
+++ client/views.py
@@ -1,9 +1,11 @@
 """Client views."""
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from client.models import Client
 
 
+@login_required
 def client_list(request):
     """Client list view."""
     clients = Client.objects.filter(created_by=request.user)
